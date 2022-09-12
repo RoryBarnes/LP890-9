61e72566fec89ca57531904f2561eae65f2abdb8
jrlivesey <jlivesey@uw.edu>
2022-09-12 00:11:41 -0700

    Modified CassiniMulti/makeplot.py for workshop sims

--- CassiniMulti/makeplot.py
+++ CassiniMulti/makeplot.py
@@ -17,40 +17,40 @@ sys.path.insert(1, str(path.parents[0]))
 
 # Run vplanet
 out = vplanet.run(path / "vpl.in", units=False)
-time = out.star.Time / 1e3
+time = out.TGstar.Time / 1e3
 
 fig = plt.figure(figsize=(6.5, 8))
 plt.subplot(3, 2, 1)
-plt.plot(time, out.b.Obliquity, color="k")
-plt.plot(time, out.c.Obliquity, color=vplot.colors.red)
+plt.plot(time, out.TGb.Obliquity, color="k")
+plt.plot(time, out.TGc.Obliquity, color=vplot.colors.red)
 plt.yscale("log")
 plt.ylabel(r"Obliquity ($^\circ$)")
 
 plt.subplot(3, 2, 2)
-plt.plot(time, out.b.Inc, color="k")
-plt.plot(time, out.c.Inc, color=vplot.colors.red)
+plt.plot(time, out.TGb.Inc, color="k")
+plt.plot(time, out.TGc.Inc, color=vplot.colors.red)
 plt.ylabel(r"Inclination ($^\circ$)")
 
 plt.subplot(3, 2, 3)
-plt.plot(time, out.b.RotPer, color="k")
-plt.plot(time, out.c.RotPer, color=vplot.colors.red)
+plt.plot(time, out.TGb.RotPer, color="k")
+plt.plot(time, out.TGc.RotPer, color=vplot.colors.red)
 plt.ylabel("Rotation Period (days)")
 
 plt.subplot(3, 2, 4)
-plt.plot(time, out.b.DynEllip, color="k", label="b")
-plt.plot(time, out.c.DynEllip, color=vplot.colors.red, label="c")
+plt.plot(time, out.TGb.DynEllip, color="k", label="b")
+plt.plot(time, out.TGc.DynEllip, color=vplot.colors.red, label="c")
 plt.ylabel("Dynamical Ellipticity")
 plt.legend(loc="upper right", fontsize=12, ncol=1)
 
 plt.subplot(3, 2, 5)
-plt.plot(time, out.b.CassiniOne, color="k")
-plt.plot(time, out.c.CassiniOne, color=vplot.colors.red)
+plt.plot(time, out.TGb.CassiniOne, color="k")
+plt.plot(time, out.TGc.CassiniOne, color=vplot.colors.red)
 plt.xlabel("Time (kyr)")
 plt.ylabel("$\sin{\Psi}$")
 
 plt.subplot(3, 2, 6)
-plt.plot(time, out.b.CassiniTwo, color="k")
-plt.plot(time, out.c.CassiniTwo, color=vplot.colors.red)
+plt.plot(time, out.TGb.CassiniTwo, color="k")
+plt.plot(time, out.TGc.CassiniTwo, color=vplot.colors.red)
 plt.xlabel("Time (kyr)")
 plt.ylabel("$\cos{\Psi}$")
 
